8345a8fed80586c0a9f44a4209c028877c128483
heyimdima <bondardima@icloud.com>
2022-06-14 21:09:59 -0800

    generated qe-code component and added routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QrCodeComponent } from './qr-code/qr-code.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: QrCodeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QrCodeComponent } from './qr-code/qr-code.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QrCodeComponent
   ],
   imports: [
     BrowserModule,
